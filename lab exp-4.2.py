8f14bdbedd0899ed41b30e41a95884750f63df46
poojitha2803 <84491810+poojitha2803@users.noreply.github.com>
2021-08-25 15:09:05 +0530

    Update lab exp-4.2.py

--- lab exp-4.2.py
+++ lab exp-4.2.py
@@ -1,3 +1,11 @@
+4.2) Write a program that asks the user to enter two strings of the same length.
+The program should then check to see if the strings are of the same length. If
+they are not, the program should print an appropriate message and exit. If they
+are of the same length, the program should alternate the characters of the two
+strings. For example, if the user enters abcde and ABCDE the program should
+print out AaBbCcDdEe
+
+program:
 s1=input("Enter any string:")
 s2=input("Enyer another string:")
 if (len(s1)!=len(s2)):
@@ -6,3 +14,7 @@ else:
     for i in range (0,len(s1)):
         print(s1[i]+s2[i],end="")
    
+Output:
+Enter any string:abcdef
+Enyer another string:ABCDEF
+aAbBcCdDeEfF    
