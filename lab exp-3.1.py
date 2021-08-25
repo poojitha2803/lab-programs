edaaf063b217eb8cdcc6dfcc3767020014c7c129
poojitha2803 <84491810+poojitha2803@users.noreply.github.com>
2021-08-25 14:02:20 +0530

    Update lab exp-3.1.py

--- lab exp-3.1.py
+++ lab exp-3.1.py
@@ -1,5 +1,21 @@
+3. Control Flow Continued
+3.1) Use a for loop to print a triangle like the one below. Allow the user to
+specify how high the triangle should be.
+*
+**
+***
+****
+
+Program:
 n=int(input("Enter the value of n:"))
 for i in range (0,n):
     for j in range (0,i):
         print("*",end=" ")
     print( )
+
+Output:
+Enter the value of n:5
+* 
+* * 
+* * * 
+* * * *     
