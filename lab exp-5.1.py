db416da86240b0a496d6702fa2bf69e2429f01fb
poojitha2803 <84491810+poojitha2803@users.noreply.github.com>
2021-08-25 15:10:31 +0530

    Update lab exp-5.1.py

--- lab exp-5.1.py
+++ lab exp-5.1.py
@@ -1,3 +1,13 @@
+5. Data structure
+5.1) Write a program that generates a list of 20 random numbers between 1 and
+100.
+(a) Print the list.
+(b) Print the average of the elements in the list.
+(c) Print the largest and smallest values in the list.
+(d) Print the second largest and second smallest entries in the list
+(e) Print how many even numbers are in the list.
+
+Program:
 import random
 a=[]
 for j in range(20):
@@ -14,3 +24,12 @@ for i in range (0,len(a)):
     if(a[i]%2==0):
         count+=1
 print("Even numbers:",count)        
+
+Output:
+list: [52, 14, 16, 8, 3, 2, 23, 62, 84, 2, 70, 76, 67, 56, 91, 12, 73, 51, 52, 21]
+Average of elements in the list: 41.75
+Largest of the list: 91
+smallest of the list: 2
+Second Largest of the list: 84
+Second smallest of the list: 2
+Even numbers: 13    
