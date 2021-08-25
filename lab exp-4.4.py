691068e0c81036a864a201a427af96496719b069
poojitha2803 <84491810+poojitha2803@users.noreply.github.com>
2021-08-25 15:59:27 +0530

    Update lab exp-4.4.py

--- lab exp-4.4.py
+++ lab exp-4.4.py
@@ -1,3 +1,10 @@
+4.4) In algebraic expressions, the symbol for multiplication is often left out, as
+in 3x+4y or 3(x+5). Computers prefer those expressions to include the
+multiplication symbol, like 3*x+4*y or 3*(x+5). Write a program that asks the
+user for an algebraic expression and then inserts multiplication symbols where
+appropriate.
+
+Program:
 exp = input("Enter algebric expression: ")
 cexp = ''
 for ch in exp:
@@ -10,3 +17,7 @@ for ch in exp:
     else:
         cexp = cexp + ch
 print("Converted expression is :",cexp)
+
+Output:
+Enter algebric expression: 3x+4y
+Converted expression is : 3*x+4*y    
