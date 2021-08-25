b0ed20bce49b3eefcd6c630af93b9d551cd807f9
poojitha2803 <84491810+poojitha2803@users.noreply.github.com>
2021-08-25 14:30:31 +0530

    Update lab exp-4.1.py

--- lab exp-4.1.py
+++ lab exp-4.1.py
@@ -1,3 +1,8 @@
+4. Strings
+4.1) Write a program that asks the user to enter a word and prints out whether
+that word contains any vowels.
+
+Program:
 word=input("Enter any word")
 for i in word:
     if i=='a' or i=='e' or i=='i' or i=='o' or i=='u' or i=='A' or i=='E' or i=='I' or i=='O' or i=='U':
@@ -5,3 +10,7 @@ for i in word:
         break
 else:
     print("Word doesnot contain vowels")
+
+Output:
+Enter any wordaditya
+Word contain vowels    
