ba7175cf6b4fc3a631566708571db00a15033fa1
poojitha2803 <84491810+poojitha2803@users.noreply.github.com>
2021-08-25 14:11:35 +0530

    Update lab exp-3.2.py

--- lab exp-3.2.py
+++ lab exp-3.2.py
@@ -1,3 +1,7 @@
+3.2) Generate a random number between 1 and 10. Ask the user to guess the
+number and print a message based on whether they get it right or not.
+
+Program:
 import random
 r=random.randint(1,10)
 guess=int(input("Enter your guess number:"))
@@ -5,3 +9,10 @@ if (r==guess):
     print("Your guess is correct")
 else:
     print("Your guess is wrong")
+
+Output:
+Enter your guess number:3
+Your guess is wrong
+
+Enter your guess number:5
+Your guess is wrong
