4d8701ab072317d23ceee2bfb7337e8a9c5785b8
poojitha2803 <84491810+poojitha2803@users.noreply.github.com>
2021-08-25 15:18:45 +0530

    Update lab exp-5.2.py

--- lab exp-5.2.py
+++ lab exp-5.2.py
@@ -1,6 +1,14 @@
+5.2) Write a program that asks the user for an integer and creates a list that
+consists of the factors of that integer.
+
+Program:
 n=int(input("Enter any number:"))
 s=[]
 for i in range(1,n+1):
     if(n%i==0):
         s.append(i)
 print(s)        
+
+Output:
+Enter any number:24
+[1, 2, 3, 4, 6, 8, 12, 24]    
