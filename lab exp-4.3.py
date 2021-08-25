0b31e405622bbcb691760e0809ff22ab87fb584d
poojitha2803 <84491810+poojitha2803@users.noreply.github.com>
2021-08-25 15:43:28 +0530

    Update lab exp-4.3.py

--- lab exp-4.3.py
+++ lab exp-4.3.py
@@ -1,4 +1,12 @@
-n=int(input("Enter a number:"))
+4.3) Write a program that asks the user for a large integer and inserts commas
+into it according to the standard American convention for commas in large
+numbers. For instance, if the user enters 1000000, the output should be
+1,000,000.
 
+Program:
+n=int(input("Enter a number:"))
 print("The number seperated with commas is:{:,}".format(n))
 
+Output:
+Enter a number:10000000
+The number seperated with commas is:10,000,000  
